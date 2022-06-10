96663a834a7791a608769fe6c1693a4dcf791582
KarachunVladislav01 <karachunvladislav@gmail.com>
2022-06-10 11:57:18 +0300

    fix: clean up code

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -7,18 +7,14 @@ import { SCREENS } from "./constants";
 
 import ScrollToTop from "./ScrollToTop";
 
-const Navigation = () => {
-  console.log("version with deploy version 3.2");
-  return (
-    <ScrollToTop>
-      <Routes>
-        <Route path={SCREENS.HOME} element={<Home />} />
-        <Route path={SCREENS.PRIVACY} element={<Privacy />} />
+const Navigation = () => (
+  <ScrollToTop>
+    <Routes>
+      <Route path={SCREENS.HOME} element={<Home />} />
+      <Route path={SCREENS.PRIVACY} element={<Privacy />} />
 
-        <Route path="*" element={<Navigate to={SCREENS.HOME} replace />} />
-      </Routes>
-    </ScrollToTop>
-  );
-};
-
+      <Route path="*" element={<Navigate to={SCREENS.HOME} replace />} />
+    </Routes>
+  </ScrollToTop>
+);
 export default Navigation;
